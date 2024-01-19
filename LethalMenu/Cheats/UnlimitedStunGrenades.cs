afb84a8d40a1e258e8c10044803b73eff953b28b
Dustin21335 <107526614+Dustin21335@users.noreply.github.com>
2024-01-18 21:57:32 -0500

    Create UnlimitedStunGrenades.cs

---
+++ LethalMenu/Cheats/UnlimitedStunGrenades.cs
@@ -0,0 +1,27 @@
+using System.Reflection;
+using HarmonyLib;
+
+
+namespace LethalMenu.Cheats
+{
+    internal class UnlimitedStunGrenades : Cheat
+    {
+        [HarmonyPatch(typeof(StunGrenadeItem), "ItemActivate")]
+        public class StunGrenadeItem_ItemActivate_Patch
+        {
+            public static bool Prefix(StunGrenadeItem __instance)
+            {
+                if (!Hack.UnlimitedStunGrenades.IsEnabled())
+                {
+                    __instance.itemUsedUp = false;
+                    __instance.pinPulled = false;
+                    __instance.hasExploded = false;
+                    __instance.DestroyGrenade = false;
+                    typeof(StunGrenadeItem).GetField("pullPinCoroutine", BindingFlags.NonPublic | BindingFlags.Instance)?.SetValue(__instance, null);
+                }
+
+                return true;
+            }
+        }
+    }
+}
