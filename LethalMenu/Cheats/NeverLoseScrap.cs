db883751e1e0aeb62e4dade492bbe1d5ee811073
Dustin21335 <107526614+Dustin21335@users.noreply.github.com>
2024-02-01 22:46:54 -0500

    Create NeverLoseScrap.cs

---
+++ LethalMenu/Cheats/NeverLoseScrap.cs
@@ -0,0 +1,18 @@
+namespace LethalMenu.Cheats
+{
+    internal class NeverLoseScrap : Cheat
+    {
+        public override void Update()
+        {
+            KeepItems();
+        }
+
+        public void KeepItems()
+        {
+            if (Hack.NeverLoseScrap.IsEnabled() && StartOfRound.Instance.allPlayersDead)
+            {
+                StartOfRound.Instance.allPlayersDead = false;
+            }
+        }
+    }
+}
