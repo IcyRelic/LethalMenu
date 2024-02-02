f1f5c2fa7288b0c9877dff9850780a49d64cf820
Dustin21335 <107526614+Dustin21335@users.noreply.github.com>
2024-02-02 17:55:07 -0500

    Update HearEveryone.cs

--- LethalMenu/Cheats/HearEveryone.cs
+++ LethalMenu/Cheats/HearEveryone.cs
@@ -15,15 +15,31 @@ namespace LethalMenu.Cheats
                 for (int i = 0; i < __instance.allPlayerScripts.Length; i++)
                 {
                     PlayerControllerB playerControllerB = __instance.allPlayerScripts[i];
-                    AudioSource currentVoiceChatAudioSource = playerControllerB.currentVoiceChatAudioSource;
-                    currentVoiceChatAudioSource.GetComponent<AudioLowPassFilter>().enabled = false;
-                    currentVoiceChatAudioSource.GetComponent<AudioHighPassFilter>().enabled = false;
-                    currentVoiceChatAudioSource.panStereo = 0f;
-                    SoundManager.Instance.playerVoicePitchTargets[(int)((IntPtr)playerControllerB.playerClientId)] = 1f;
-                    SoundManager.Instance.SetPlayerPitch(1f, unchecked((int)playerControllerB.playerClientId));
-                    currentVoiceChatAudioSource.spatialBlend = 0f;
-                    playerControllerB.currentVoiceChatIngameSettings.set2D = true;
-                    playerControllerB.voicePlayerState.Volume = 1f;
+
+                    if (playerControllerB != null && playerControllerB.currentVoiceChatAudioSource != null)
+                    {
+                        AudioSource currentVoiceChatAudioSource = playerControllerB.currentVoiceChatAudioSource;
+
+                        AudioLowPassFilter lowPassFilter = currentVoiceChatAudioSource.GetComponent<AudioLowPassFilter>();
+                        AudioHighPassFilter highPassFilter = currentVoiceChatAudioSource.GetComponent<AudioHighPassFilter>();
+
+                        if (lowPassFilter != null)
+                        {
+                            lowPassFilter.enabled = false;
+                        }
+
+                        if (highPassFilter != null)
+                        {
+                            highPassFilter.enabled = false;
+                        }
+
+                        currentVoiceChatAudioSource.panStereo = 0f;
+                        SoundManager.Instance.playerVoicePitchTargets[(int)((IntPtr)playerControllerB.playerClientId)] = 1f;
+                        SoundManager.Instance.SetPlayerPitch(1f, unchecked((int)playerControllerB.playerClientId));
+                        currentVoiceChatAudioSource.spatialBlend = 0f;
+                        playerControllerB.currentVoiceChatIngameSettings.set2D = true;
+                        playerControllerB.voicePlayerState.Volume = 1f;
+                    }
                 }
             }
         }
