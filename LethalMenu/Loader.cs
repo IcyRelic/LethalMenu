2287455ba89188c55da1e6d36beaee34106aed0e
Dustin21335 <107526614+Dustin21335@users.noreply.github.com>
2024-04-27 20:31:32 -0400

    Update Loader.cs

--- LethalMenu/Loader.cs
+++ LethalMenu/Loader.cs
@@ -1,4 +1,4 @@
-﻿using System.IO;
+using System.IO;
 using System.Reflection;
 using System;
 using UnityEngine;
@@ -14,13 +14,13 @@ namespace LethalMenu
 
         public static void Init()
         {
-
-            LoadHarmony();
-            Loader.Load = new GameObject();
-            Load.AddComponent<LethalMenu>();
-            Object.DontDestroyOnLoad(Loader.Load);
-
-           
+            if (Load == null)
+            {
+                LoadHarmony();
+                Loader.Load = new GameObject();
+                Load.AddComponent<LethalMenu>();
+                Object.DontDestroyOnLoad(Loader.Load);
+            }
         }
 
         public static void LoadHarmony()
@@ -30,13 +30,10 @@ namespace LethalMenu
             Stream stream = assembly.GetManifestResourceStream(name);
             byte[] rawAssembly = new byte[stream.Length];
             stream.Read(rawAssembly, 0, (int)stream.Length);
-
             AppDomain.CurrentDomain.Load(rawAssembly);
             harmonyLoaded = true;
         }
 
-
         public static void Unload() => Object.Destroy(Load);
-
     }
 }
