cb5914aa7c680507ff2f43f8664fa24662b451af
Dustin21335 <107526614+Dustin21335@users.noreply.github.com>
2024-07-04 01:53:37 -0400

    Update NeverLoseScrap.cs

--- LethalMenu/Cheats/NeverLoseScrap.cs
+++ LethalMenu/Cheats/NeverLoseScrap.cs
@@ -1,4 +1,5 @@
 using HarmonyLib;
+using Steamworks.Ugc;
 using Unity.Netcode;
 using UnityEngine;
 
@@ -9,45 +10,18 @@ namespace LethalMenu.Cheats
         [HarmonyPatch(typeof(RoundManager), ("DespawnPropsAtEndOfRound"))]
         public static class DespawnPropsAtEndOfRoundPatch
         {
-            public static bool Prefix(ref RoundManager __instance, bool despawnAllItems = false)
+            public static bool Prefix(bool despawnAllItems = false)
             {
-                if (Hack.NeverLoseScrap.IsEnabled())
                 {
-                    if (!(__instance).IsServer)
+                    foreach (var item in LethalMenu.items)
                     {
-                        return false;
-                    }
-
-                    GrabbableObject[] array = Object.FindObjectsOfType<GrabbableObject>();
-                    for (int i = 0; i < array.Length; i++)
-                    {
-                        if (despawnAllItems || (!array[i].isHeld && !array[i].isInShipRoom) || array[i].deactivated)
+                        if (Hack.NeverLoseScrap.IsEnabled() && item.isInShipRoom)
                         {
-                            if (array[i].isHeld && (Object)(object)array[i].playerHeldBy != (Object)null)
-                            {
-                                array[i].playerHeldBy.DropAllHeldItems(true, false);
-                            }
-                            array[i].gameObject.GetComponent<NetworkObject>().Despawn(true);
+                            return false;
                         }
-                        else
-                        {
-                            array[i].scrapPersistedThroughRounds = true;
-                        }
-
-                        if (__instance.spawnedSyncedObjects.Contains(array[i].gameObject))
-                        {
-                            __instance.spawnedSyncedObjects.Remove(array[i].gameObject);
-                        }
-                    }
-
-                    GameObject[] array2 = GameObject.FindGameObjectsWithTag("TemporaryEffect");
-                    for (int j = 0; j < array2.Length; j++)
-                    {
-                        Object val = (Object)(object)array2[j];
-                        Object.Destroy(val);
                     }
                 }
-                return false;
+                return true;
             }
         }
     }
