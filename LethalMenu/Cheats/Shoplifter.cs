fea71752e76301bf708c4965d267db49931657ce
IcyRelic <william@icyrelic.com>
2024-08-08 21:10:05 -0400

    Fixed shoplifter to include vehicles

--- LethalMenu/Cheats/Shoplifter.cs
+++ LethalMenu/Cheats/Shoplifter.cs
@@ -1,52 +1,77 @@
 using HarmonyLib;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
 
 namespace LethalMenu.Cheats
 {
-    [HarmonyPatch(typeof(Terminal), "Update")]
+    [HarmonyPatch]
     internal class Shoplifter : Cheat
     {
-        public static float[] originalCreditsWorths;
-        [HarmonyPrefix]
-        public static void Prefix(ref Item[] ___buyableItemsList)
+        public static Dictionary<string, int> nodeCosts = new Dictionary<string, int>();
+        public static int[] itemCosts;
+
+
+        public override void Update()
         {
+            Terminal term = Object.FindAnyObjectByType<Terminal>();
+            TerminalKeyword buy = term.terminalNodes.allKeywords.ToList().Find(x => x.word == "buy");
+
+            
+
+            if(Hack.Shoplifter.IsEnabled())
+            {
+                buy.compatibleNouns.ToList().ForEach(n =>
+                {
+                    if (!nodeCosts.ContainsKey(n.noun.name)) nodeCosts.Add(n.noun.name, n.result.itemCost);
+
+                    n.result.itemCost = 0;
+                });
+
+                if(itemCosts is null)
+                {
+                    itemCosts = new int[term.buyableItemsList.Length];
+                    for (int i = 0; i < term.buyableItemsList.Length; i++)
+                    {
+                        itemCosts[i] = term.buyableItemsList[i].creditsWorth;
+                    }
+                }
+
+                term.buyableItemsList.ToList().ForEach(i => i.creditsWorth = 0);
+            }
+            else if(nodeCosts.Count > 0)
+            {
+                buy.compatibleNouns.ToList().FindAll(n => nodeCosts.ContainsKey(n.noun.name)).ForEach(n => n.result.itemCost = nodeCosts[n.noun.name]);
+
+                for (int i = 0; i < term.buyableItemsList.Length; i++)
+                {
+                    term.buyableItemsList[i].creditsWorth = itemCosts[i];
+                }
+
+                Clear();
+            }
+
+            
+        }
+
+        public static void Clear()
+        {
+            nodeCosts.Clear();
+            itemCosts = null;
+        }
+
+        [HarmonyPrefix]
+        [HarmonyPatch(typeof(Terminal), "LoadNewNodeIfAffordable")]
+        public static void LoadNode(TerminalNode node)
+        {
+            
             if (Hack.Shoplifter.IsEnabled())
             {
-                if (___buyableItemsList == null)
-                    return;
-
-                if (originalCreditsWorths == null || originalCreditsWorths.Length != ___buyableItemsList.Length)
-                {
-                    originalCreditsWorths = new float[___buyableItemsList.Length];
-                    for (int i = 0; i < ___buyableItemsList.Length; i++)
-                    {
-                        if (___buyableItemsList[i] != null)
-                        {
-                            originalCreditsWorths[i] = ___buyableItemsList[i].creditsWorth;
-                        }
-                    }
-                }
-                for (int index = 0; index < ___buyableItemsList.Length; ++index)
-                {
-                    if (___buyableItemsList[index] != null)
-                    {
-                        ___buyableItemsList[index].creditsWorth = 0;
-                    }
-                }
-            }
-            else
-            {
-                if (___buyableItemsList == null)
-                    return;
-
-                for (int index = 0; index < ___buyableItemsList.Length; ++index)
-                {
-                    if (___buyableItemsList[index] != null && originalCreditsWorths != null && originalCreditsWorths.Length > index)
-                    {
-                        ___buyableItemsList[index].creditsWorth = (int)originalCreditsWorths[index];
-                    }
-                }
-                originalCreditsWorths = null;
+                Debug.Log($"Making Item Free {node.name}");
+                node.itemCost = 0;
             }
         }
+
     }
+        
 }
