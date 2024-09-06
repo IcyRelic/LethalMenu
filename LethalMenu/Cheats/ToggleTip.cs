9895821835330a32fd071f7791aa6e052a99f61a
Dustin21335 <107526614+Dustin21335@users.noreply.github.com>
2024-09-06 17:36:57 -0400

    Update ToggleTip.cs

--- LethalMenu/Cheats/ToggleTip.cs
+++ LethalMenu/Cheats/ToggleTip.cs
@@ -1,4 +1,4 @@
-﻿using System.Linq;
+using System.Linq;
 using LethalMenu.Menu.Popup;
 using TMPro;
 
@@ -8,10 +8,15 @@ namespace LethalMenu.Cheats
     {
         public override void Update()
         {
-            if (HUDManager.Instance == null || !Hack.ToggleTip.IsEnabled()) return;
+            if (HUDManager.Instance == null) return;
             string ToggleKey = $"Toggle Menu : {FirstSetupManagerWindow.kb}";
             TextMeshProUGUI ToggleTipLine = HUDManager.Instance.controlTipLines.FirstOrDefault(l => l.text.Contains(ToggleKey));
-            if (ToggleTipLine != null || !Hack.ToggleTip.IsEnabled()) ToggleTipLine.text = "";
+            if (!Hack.ToggleTip.IsEnabled())
+            {
+                if (ToggleTipLine != null) ToggleTipLine.text = ""; 
+                return;
+            }
+            if (ToggleTipLine != null) ToggleTipLine.text = "";
             TextMeshProUGUI FirstEmptyLine = HUDManager.Instance.controlTipLines.FirstOrDefault(l => string.IsNullOrEmpty(l.text));
             if (FirstEmptyLine != null) FirstEmptyLine.text = ToggleKey;
         }
