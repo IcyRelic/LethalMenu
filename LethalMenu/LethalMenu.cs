7ac0aec4128f1c5672bb729f6b3202310cd89a94
Dustin21335 <107526614+Dustin21335@users.noreply.github.com>
2024-09-06 16:53:43 -0400

    Update LethalMenu.cs

--- LethalMenu/LethalMenu.cs
+++ LethalMenu/LethalMenu.cs
@@ -155,8 +155,8 @@ namespace LethalMenu
             {
                 if (Event.current.type == EventType.Repaint)
                 {
-                    string LethalMenuTitle = $"Lethal Menu {Settings.version} By IcyRelic, and Dustin | Discord.gg/HzxykGKA6P |";
-                    LethalMenuTitle += Settings.b_FPSCounter ? $" FPS: {fps}" : "";
+                    string LethalMenuTitle = $"Lethal Menu {Settings.version} By IcyRelic, and Dustin | ";
+                    LethalMenuTitle += Settings.b_FPSCounter ? $"FPS: {fps}" : "";
                     VisualUtil.DrawString(new Vector2(5f, 2f), LethalMenuTitle, Settings.c_primary, centered: false, bold: true, fontSize: 14);
                     if (MenuUtil.resizing)
                     {
