c7a0cf87e6318a6d4f1e9bfcaf8b36a306bf3e7c
Dustin21335 <107526614+Dustin21335@users.noreply.github.com>
2024-09-18 08:58:59 -0400

    Update DoublewingController.cs

--- LethalMenu/Handler/EnemyControl/DoublewingController.cs
+++ LethalMenu/Handler/EnemyControl/DoublewingController.cs
@@ -4,22 +4,17 @@ namespace LethalMenu.Handler.EnemyControl
 {
     internal class DoublewingController : IEnemyController<DoublewingAI>
     {
-        // haven't tested yet
         public void UsePrimarySkill(DoublewingAI enemy)
-        {
-            enemy.AlertBirdServerRpc();
-        }
-
-        public void UseSecondarySkill(DoublewingAI enemy)
         {
             enemy.Reflect().Invoke("BirdScreech");
         }
 
-        public string GetPrimarySkillName(DoublewingAI _)
+        public void UseSecondarySkill(DoublewingAI enemy)
         {
-            return _.Reflect().GetValue<bool>("alertingBird") ? "Alert Bird" : "Screech (Right Click)";
         }
-        public string GetSecondarySkillName(DoublewingAI _) => "Screech";
+
+        public string GetPrimarySkillName(DoublewingAI _) => "Screech";
+        public string GetSecondarySkillName(DoublewingAI _) => "";
         public bool CanUseEntranceDoors(DoublewingAI _) => false;
         public float InteractRange(DoublewingAI _) => 2.5f;
     }
